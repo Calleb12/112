7e1ffeaeae722c91792788d479cb6e8a88e34618
Calleb12 <116227059+Calleb12@users.noreply.github.com>
2023-09-20 18:43:22 -0300

    Add files via upload

---
+++ organise.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+from_dir = "C:/Users/User/Downloads"
+
+to_dir = "C:/Users/User/Downloads/imagensbaixadas"
+
+list_of_list = os.listdir(from_dir)
+print(list_of_list)
+
+
+for file_name in list_of_list:
+    name,extension = os.path.splitext(file_name)
+    #print(extension)
+
+    if extension == '':
+        continue
+    if extension in ['.txt', '.doc', '.docx', '.pdf']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "arquivo imagem"
+        path3 = to_dir + '/' + "arquivo imagem" + '/' + file_name
+
+        if os.path.exists(path2):
+            print("movendo" + file_name + ".....")
+
+            shutil.move(path1, path3)
+
+        else: 
+            os.makedirs(path2)
+            print("movendo" + file_name + "......")
+            shutil.move(path1, path3)
